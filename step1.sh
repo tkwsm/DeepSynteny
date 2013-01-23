b735992a45071e39e36ba6ec799a84df2e6f1b5d
tkwsm <kawashima38@gmail.com>
2013-01-23 11:19:12 +0900

    add step1.sh

---
+++ step1.sh
@@ -0,0 +1,2 @@
+
+ruby syntenic_step1.rb ../Nemve1.FilteredModels1.mod.gff3 ../Nemve1.fasta ../aug_repeatmask_pasa_input.gff3 ../adi-scaffold_newbler-sopra101124.mod.fa ../nv2ad2nv.mutual.mod.list out  >out.log
